288858453c15b44bd13970d04601c250e47d01cb
sammedchougule <smdchougule24@gmail.com>
2024-10-26 20:57:38 +0530

    "changed grid system to flex boxes"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import StockData from './stockData';
 import './App.css';
+//import TopMovers from './components/topMovers';
+import TopMovers from './topMovers';
+
 
 
 function App() {
   return (
     <div className="App">
       <StockData />
+      <TopMovers />
+
     </div>
   );
 }
